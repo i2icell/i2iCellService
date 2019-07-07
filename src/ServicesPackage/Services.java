4205db0f7339f8f0c32a47031aa38b5095232f57
Davut Kurt <51950119+davutkurt@users.noreply.github.com>
2019-07-08 01:23:56 +0300

    Update Services.java

--- src/ServicesPackage/Services.java
+++ src/ServicesPackage/Services.java
@@ -2,16 +2,28 @@ package ServicesPackage;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Types;
 
 public class Services {
+	private Connector connector;
+	private Connection connection;
+	
+	public Connector getConnector() {
+		if( this.connector == null )
+			this.connector = new Connector();
+		return connector;
+	}
+
+	public Connection getConnection() {
+		if( this.connection == null )
+			this.connection = this.getConnector().connect();
+		return connection;
+	}
 
 	private boolean phoneNumberVerification(String inputPhoneNumber) {
 	if( String.valueOf(inputPhoneNumber).startsWith("5") ) {
-		String pattern = "(?=.*[0-9]).{10}";
+		String pattern = "\\d{10}$";
 	    return inputPhoneNumber.matches(pattern);
 		}
 	return false;		
@@ -22,27 +34,23 @@ public class Services {
 	    return inputPassword.matches(pattern);
 	}
 	
-	private boolean firstNameVerification(String inputFirstName) {
-		String pattern = "([a-zA-Z]*).{2,}";
-	    return inputFirstName.matches(pattern);
-	}
-	
-	private boolean lastNameVerification(String inputLastName) {
-	    String pattern = "([a-zA-Z]*).{2,}";
-	    return inputLastName.matches(pattern);
+	private boolean nameVerification(String inputName) {
+		
+		String pattern = "([a-zA-Z]){2,}";
+	    return inputName.matches(pattern);
 	}
 	
 	private boolean emailVerification(String inputEmail) {
+		
 		String pattern ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	return inputEmail.matches(pattern) ;
 	}
 	
-	private boolean TcNumberVerification(String inputTcNumber) {
-		String pattern = "(\\d+)";
-		String lengthPattern = ".{11}";
-		if( inputTcNumber.matches(pattern) && inputTcNumber.matches(lengthPattern) ) {
-			
+	private boolean tcNumberVerification(String inputTcNumber) {
+		String pattern = "\\d{11}$";
+		if( inputTcNumber.matches(pattern) ) {
+
 		    char[] chars = inputTcNumber.toCharArray();
 		    int[] temporaryTcNumber = new int[11];
 		    
@@ -50,34 +58,37 @@ public class Services {
 		    	temporaryTcNumber[i] = chars[i] - '0';
 		    }
 	
-		    if(temporaryTcNumber[0] == 0) { 
+		    if(temporaryTcNumber[0] == 0 || temporaryTcNumber[10] % 2 == 1 ) { 
 		        return false;
 		    }
-		    if(temporaryTcNumber[10] % 2 == 1) {
-		    	return false;
-		    }
-	
+
 		    if(((temporaryTcNumber[0] + temporaryTcNumber[2] + temporaryTcNumber[4] + temporaryTcNumber[6] + 
 		        	temporaryTcNumber[8]) * 7 - (temporaryTcNumber[1] + temporaryTcNumber[3] + temporaryTcNumber[5] + 
 		        			temporaryTcNumber[7])) % 10 != temporaryTcNumber[9]) { 
 		        return false;
 		    }
-	
+
 		    if((temporaryTcNumber[0] + temporaryTcNumber[1] + temporaryTcNumber[2] + temporaryTcNumber[3] + 
 		        	temporaryTcNumber[4] + temporaryTcNumber[5] + temporaryTcNumber[6] + temporaryTcNumber[7] + 
 		        		temporaryTcNumber[8] + temporaryTcNumber[9]) % 10 != temporaryTcNumber[10]) {
 		       return false;
 		    }
+
 		    return true;
 		}else {
 			return false;
 		}
-	        
 	}
 	
-	private Date changeTypeOfDate(String birthDate) {
+	private boolean checkUserInfo(String inputFirstName,    String inputLastName,  String inputPhoneNumber,
+				      String inputEmail,        String inputPassword,  String inputTcNumber) {
 		
-	    return Date.valueOf(birthDate);
+		
+		if( !nameVerification(inputFirstName)    || !nameVerification(inputLastName) || !phoneNumberVerification(inputPhoneNumber)|| 
+			!passwordVerification(inputPassword) || !emailVerification(inputEmail)   || !tcNumberVerification(inputTcNumber)) {
+			return false;
+		}
+		return true;
 	}
 	
 	public int login(String inputPhoneNumber, String inputPassword) {
@@ -87,12 +98,10 @@ public class Services {
 		}
 		
 		int result ;
-		Connection connectionDB = null;
 		CallableStatement csProcedureObject = null;
 		try {
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			connectionDB = DriverManager.getConnection("jdbc:oracle:thin:@68.183.75.84:49161:xe", "SYSTEM","oracle");
-			csProcedureObject = connectionDB.prepareCall("{CALL CHECK_USER(?,?,?)}");
+			
+			csProcedureObject = this.getConnection().prepareCall("{CALL CHECK_USER(?,?,?)}");
 
 			csProcedureObject.setString(1, inputPhoneNumber);
 			csProcedureObject.setString(2, inputPassword);
@@ -110,7 +119,7 @@ public class Services {
 			
 			try {
 				csProcedureObject.close();
-				connectionDB.close();
+				connection.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -120,35 +129,22 @@ public class Services {
 	}
 
 	public int createAccount(String inputFirstName,    String inputLastName,   
-							 String inputPhoneNumber,  String inputEmail, 
-							 String inputPassword,     String inputBirthDate,
-							 Double inputTcNumber) {
+				 String inputPhoneNumber,  String inputEmail, 
+				 String inputPassword,     String inputBirthDate,
+				 String inputTcNumber) {
+		
+		
+		if(!checkUserInfo( inputFirstName, inputLastName,  inputPhoneNumber,
+				   inputEmail,     inputPassword,  inputTcNumber)) {
+			return 0;
+		}
 
-		if( !firstNameVerification(inputFirstName) || !lastNameVerification(inputLastName) || 
-				!phoneNumberVerification(inputPhoneNumber) || !passwordVerification(inputPassword) || 
-					!emailVerification(inputEmail)) {
-			return 0;
-		}
-		
-		
-		/* TC Numarasi DB de Hala Number tipinde olduðundan dolayi suan eklenmedi ve tipi degistirilmedi. 
-		   Daha sonra tipi değiştirilip verification eklenecek.
-		    if( !TcNumberVerification(inputTcNumber) ) {
-			return 0;
-		}
-		*/
-		Date birthDate = changeTypeOfDate(inputBirthDate);
-		
-		Connection connectionDB = null;
 		CallableStatement csProcedureObject = null;
-		int result;
 		
+		int result;
 		try {
 			
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			
-			connectionDB = DriverManager.getConnection("jdbc:oracle:thin:@68.183.75.84:49161:xe", "SYSTEM","oracle");
-			csProcedureObject = connectionDB.prepareCall("{CALL CREATE_USER(?,?,?,?,?,?,?,?)}");
+			csProcedureObject = this.getConnection().prepareCall("{CALL CREATE_USER(?,?,?,?,?,?,?,?)}");
 			
 			csProcedureObject.registerOutParameter(8, Types.INTEGER);
 			csProcedureObject.setString(1, inputFirstName);
@@ -156,8 +152,8 @@ public class Services {
 			csProcedureObject.setString(3, inputPhoneNumber);
 			csProcedureObject.setString(4, inputEmail);
 			csProcedureObject.setString(5, inputPassword);
-			csProcedureObject.setDate(6, birthDate);
-			csProcedureObject.setDouble(7, inputTcNumber);
+			csProcedureObject.setString(6, inputBirthDate);
+			csProcedureObject.setString(7, inputTcNumber);
 
 			csProcedureObject.execute();
 			result = csProcedureObject.getInt(8);
@@ -168,7 +164,7 @@ public class Services {
 		} finally {
 			
 			try {
-				connectionDB.close();
+				connection.close();
 				csProcedureObject.close();
 			} catch (SQLException e) {
 				
@@ -182,12 +178,10 @@ public class Services {
 	public Integer[] getBalances(String inputPhoneNumber) {
 		Integer[] remainResults = new Integer[3];
 		
-		Connection connectionDB = null;
 		CallableStatement csProcedureObject = null;
 		try {
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			connectionDB = DriverManager.getConnection("jdbc:oracle:thin:@68.183.75.84:49161:xe", "SYSTEM","oracle");
-			csProcedureObject = connectionDB.prepareCall("{CALL GET_BALANCES(?,?,?,?)}");
+			
+			csProcedureObject = this.getConnection().prepareCall("{CALL GET_BALANCES(?,?,?,?)}");
 		
 			csProcedureObject.setString(1, inputPhoneNumber);
 			
@@ -209,7 +203,7 @@ public class Services {
 			
 			try {
 				csProcedureObject.close();
-				connectionDB.close();
+				connection.close();
 			} catch (SQLException e) {
 				
 				e.printStackTrace();
@@ -219,31 +213,28 @@ public class Services {
 		return remainResults;
 	}
 	
-	
-	public int changePassword(String inputPhoneNumber, Double inputTcNumber,
+	public int changePassword(String inputPhoneNumber, String inputTcNumber,
 							  String inputNewPassword ) {
 		
-		if( !phoneNumberVerification(inputPhoneNumber) || !passwordVerification(inputNewPassword)) {
+		if( !phoneNumberVerification(inputPhoneNumber) || !passwordVerification(inputNewPassword) || 
+				!tcNumberVerification(inputTcNumber)) {
 			return 0;
 		}
 		
 		int result ;
-		Connection connectionDB = null;
 		CallableStatement csProcedureObject = null;
+		
 		try {
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			connectionDB = DriverManager.getConnection("jdbc:oracle:thin:@68.183.75.84:49161:xe", "SYSTEM","oracle");
-			csProcedureObject = connectionDB.prepareCall("{CALL CHANGE_PASSWORD(?,?,?,?)}");
-
+			
+			csProcedureObject = this.getConnection().prepareCall("{CALL CHANGE_PASSWORD(?,?,?,?)}");
 			csProcedureObject.setString(1, inputPhoneNumber);
-			csProcedureObject.setDouble(2, inputTcNumber);
+			csProcedureObject.setString(2, inputTcNumber);
 			csProcedureObject.setString(3, inputNewPassword);
 			csProcedureObject.registerOutParameter(4, Types.INTEGER);
 			
 			csProcedureObject.execute();
 			
 			result = csProcedureObject.getInt(4);
-			
 			
 		} catch (SQLException ex) {
 			ex.getMessage();
@@ -252,12 +243,14 @@ public class Services {
 			
 			try {
 				csProcedureObject.close();
-				connectionDB.close();
+				connection.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}		
 		}
 		return result;
 	}
+
+	
 
 }
